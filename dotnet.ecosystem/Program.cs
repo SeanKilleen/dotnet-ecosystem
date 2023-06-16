b63cfc141d45e7eacf4dd513e8391dd5052abbef
Sean Killeen <SeanKilleen@gmail.com>
2023-06-15 23:02:06 -0400

    Basic actors

--- dotnet.ecosystem/Program.cs
+++ dotnet.ecosystem/Program.cs
@@ -1,33 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.CommandLine;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using Akka.Actor;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
+namespace dotnet.ecosystem;
 
-class Program
+public class Messages
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    public record FindProjects(string Path);
+    public record ProcessProject(FileInfo File);
 }
 
-internal sealed class EcosystemCommand : Command<EcosystemCommand.Settings>
+public class CsProjectProcessor : ReceiveActor
 {
-
-    public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
+    public CsProjectProcessor()
     {
-        AnsiConsole.MarkupLine($"[red]R[/][green]G[/][blue]B[/] - Scan Folder is [yellow]{settings.ScanFolder}[/]");
-
-
-        return 0;
+        Receive<Messages.ProcessProject>(msg =>
+        {
+            AnsiConsole.Markup($"Processing [yellow]{msg.File.FullName}[/]");
+        });
+    }
+}
+public class ProjFinderActor : ReceiveActor
+{
+    public ProjFinderActor()
+    {
+        var csProjProcessor = Context.ActorSelection("../csProjProcessor");
+        Receive<Messages.FindProjects>(msg =>
+        {
+            AnsiConsole.WriteLine($"Checking {msg.Path} for files");
+            var files = Directory.GetFiles(msg.Path, "*.csproj", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                AnsiConsole.WriteLine($"Found {file}");
+                var fileInfo = new FileInfo(file);
+                csProjProcessor.Tell(new Messages.ProcessProject(fileInfo));
+            }
+        });
     }
 
-    public sealed class Settings : CommandSettings
+}
+public class Program
+{
+    public static async Task<int> Main(string[] args)
     {
-        [CommandArgument(0, "<scanFolder>")]
-        public string ScanFolder { get; set; }
+        string ScanFolder = args[0]; // TODO: Real command parsing & syntax
+
+        AnsiConsole.MarkupLine($"[red]R[/][green]G[/][blue]B[/] - Scan Folder is [yellow]{ScanFolder}[/]");
+
+        var system = ActorSystem.Create("dotnet-ecosystem");
+
+        var finder = system.ActorOf<ProjFinderActor>("projFinder");
+        var processor = system.ActorOf<CsProjectProcessor>("csProjProcessor");
+
+        finder.Tell(new Messages.FindProjects(ScanFolder));
+
+        Console.ReadLine();
+
+        return await Task.FromResult<int>(0);
     }
 }
