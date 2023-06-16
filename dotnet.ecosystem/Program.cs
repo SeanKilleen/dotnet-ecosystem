282c937ae873b5609fcf9d6d1a389892ac442895
Sean Killeen <SeanKilleen@gmail.com>
2023-06-15 21:29:31 -0400

    basics

--- dotnet.ecosystem/Program.cs
+++ dotnet.ecosystem/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+var app = new CommandApp<EcosystemCommand>();
+
+return app.Run(args);
 Console.WriteLine("Hello, World!");
+
+
+internal sealed class EcosystemCommand : Command<EcosystemCommand.Settings>
+{
+
+    public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
+    {
+        AnsiConsole.MarkupLine("[red]R[/][green]G[/][blue]B[/]");
+        return 0;
+    }
+
+    public sealed class Settings : CommandSettings
+    {
+
+    }
+}
